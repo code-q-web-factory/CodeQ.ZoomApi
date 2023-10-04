2f3b8aff002b8ec800dd62c4be142e95c11b0bf2
Felix Gradinaru <felix@gradinaru.me>
2023-10-04 10:13:37 +0200

    Bump minimum php version to 8.1, adapt ZoomApiAccessToken constructor to new syntax

--- Classes/Domain/Model/ZoomApiAccessToken.php
+++ Classes/Domain/Model/ZoomApiAccessToken.php
@@ -4,16 +4,8 @@ namespace CodeQ\ZoomApi\Domain\Model;
 
 class ZoomApiAccessToken
 {
-    public readonly string $accessToken;
-    public readonly array $scope;
-
-    /**
-     * @param string $accessToken
-     * @param array  $scope
-     */
-    public function __construct(string $accessToken, array $scope)
-    {
-        $this->accessToken = $accessToken;
-        $this->scope = $scope;
-    }
+    public function __construct(
+        public readonly string $accessToken,
+        public readonly array $scope
+    ) {}
 }
